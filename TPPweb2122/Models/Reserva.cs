a320077729f9af314d5a6af8fe9a512f0c18d405
Diogo Pascoal <diogopascoal789@gmail.com>
2022-01-18 00:24:27 +0000

    Create Class Reserva

--- TPPweb2122/Models/Reserva.cs
+++ TPPweb2122/Models/Reserva.cs
@@ -1,6 +1,14 @@
-﻿namespace TPPweb2122.Models
+﻿using System;
+
+namespace TPPweb2122.Models
 {
     public class Reserva
     {
+        public int ReservaId { get; set; }
+        public DateTime dataEntrada { get; set; }   
+        public DateTime dataSaida { get; set; } 
+
+        public int ImovelId { get; set; }
+        public virtual Imovel Imovel {get; set; }
     }
 }
