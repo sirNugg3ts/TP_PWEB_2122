b6b64895bce08a9e1da8b77de1378921fb3083e2
Nuno Honório <nuno.honorio2000@gmail.com>
2022-01-20 13:21:13 +0000

    Register Entities Fixed

--- TPPweb2122/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TPPweb2122/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,17 +88,25 @@ namespace TPPweb2122.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
+            var user = new Utilizador();
+            var role = Roles.RoleUtils.RegistSelectList.FirstOrDefault(s => s.Value == Input.RoleId.ToString());
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new Utilizador { UserName = Input.Email, Email = Input.Email , Nome = Input.Nome, Morada = Input.Morada, Telefone = Input.Telefone, };
-         
+                if(role.Text == "Cliente")
+                {
+                    user = new Cliente { UserName = Input.Email, Email = Input.Email, Nome = Input.Nome, Morada = Input.Morada, Telefone = Input.Telefone, };
+                }else if (role.Text == "Gestor")
+                {
+                    user = new Gestor { UserName = Input.Email, Email = Input.Email, Nome = Input.Nome, Morada = Input.Morada, Telefone = Input.Telefone, };
+                }
+               
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var role = Roles.RoleUtils.RegistSelectList.FirstOrDefault(s => s.Value == Input.RoleId.ToString());
+                    role = Roles.RoleUtils.RegistSelectList.FirstOrDefault(s => s.Value == Input.RoleId.ToString());
                     
                     await _userManager.AddToRoleAsync(user, role.Text);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
